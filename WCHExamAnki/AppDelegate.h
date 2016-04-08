a66ee8be80a6ca1d160d8e3758531fc42bfe33a6
Hankid <1464373159@qq.com>
2016-04-08 15:58:57 +0800

    Initial Commit

---
+++ WCHExamAnki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCHExamAnki
+//
+//  Created by Hankid Wang on 16/4/8.
+//  Copyright © 2016年 Hankid Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCHExamAnki/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WCHExamAnki
+//
+//  Created by Hankid Wang on 16/4/8.
+//  Copyright © 2016年 Hankid Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
